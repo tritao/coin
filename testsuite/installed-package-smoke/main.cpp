80be1b9cf91b56715aa23217256c9325be037657
Joao Matos <joao@tritao.eu>
2026-08-11 15:02:06 +0100

    tests: validate profile-aware OpenGL packaging

---
+++ testsuite/installed-package-smoke/main.cpp
@@ -0,0 +1,19 @@
+#include <Inventor/SoDB.h>
+#include <Inventor/SoRenderManager.h>
+#include <Inventor/SoSceneManager.h>
+#include <Inventor/system/khronos/glcorearb.h>
+#include <Inventor/system/gl.h>
+
+int
+main()
+{
+  SoDB::init();
+  {
+    SoRenderManager renderManager;
+    SoSceneManager sceneManager;
+    (void) renderManager;
+    (void) sceneManager;
+  }
+  SoDB::finish();
+  return 0;
+}
