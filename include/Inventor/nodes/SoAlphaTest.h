9a6ec2631bd029e601bd7a5bf431d0b292412858
Joao Matos <joao@tritao.eu>
2026-08-12 09:15:50 +0100

    rendering: retain per-material geometry and alpha
    Retain alpha and generated vertex transparency alongside per-material geometry batches.

--- include/Inventor/nodes/SoAlphaTest.h
+++ include/Inventor/nodes/SoAlphaTest.h
@@ -61,6 +61,8 @@ public:
   SoSFEnum function;
   SoSFFloat value;
 
+  void doAction(SoAction * action) override;
+
 #if COIN_HAVE_LEGACY_GL_RENDERER
   void GLRender(SoGLRenderAction * action) override;
 #endif
