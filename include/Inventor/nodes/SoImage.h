54728eab691b8fb7768b2262e759f46cf45067bc
Joao Matos <joao@tritao.eu>
2026-08-15 00:14:11 +0100

    rendering: retain screen-space image commands

--- include/Inventor/nodes/SoImage.h
+++ include/Inventor/nodes/SoImage.h
@@ -43,6 +43,7 @@
 class SoSensor;
 class SoFieldSensor;
 class SbImage;
+class SoIRRenderAction;
 
 class COIN_DLL_API SoImage : public SoShape {
   typedef SoShape inherited;
@@ -75,6 +76,7 @@ public:
 #if COIN_HAVE_LEGACY_GL_RENDERER
   void GLRender(SoGLRenderAction * action) override;
 #endif
+  void IRRender(SoIRRenderAction * action) override;
   void rayPick(SoRayPickAction * action) override;
   void getPrimitiveCount(SoGetPrimitiveCountAction * action) override;
 
